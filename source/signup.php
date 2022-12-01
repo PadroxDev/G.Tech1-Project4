1f232b48d27dc685ac83323e6db2a8ef6f389d53
BlackDrift <fovet.vianney@gmail.com>
2022-12-01 10:35:23 +0100

    signup and signin almost functional

--- source/signup.php
+++
@@ -1,8 +0,0 @@
-<form method="post" action="action/signup.php">
-    <input type='email' name='email' />
-    <input type='password' name='password' />
-    <input type='text' name='username' />
-    <input type='text' name='firstname'/>
-    <input type='text' name='lastname' />
-    <input type='submit' value="Sign up" />
-</form>
